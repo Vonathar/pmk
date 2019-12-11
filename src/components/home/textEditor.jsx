f040429adbd7c2008f1c9c2bc56bd21f64988966
Gianmarco <gianmarco.caputo@wearetwogether.com>
2019-12-11 14:24:35 +0000

    Minor amends

--- src/components/home/textEditor.jsx
+++ src/components/home/textEditor.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { InputGroup, FormControl } from 'react-bootstrap';
 import '../../css/home/textEditor.css'
 
 class TextEditor extends Component {
@@ -8,6 +9,16 @@ class TextEditor extends Component {
                 <p className="editor-h1">
                     Text editor
                 </p>
+                <InputGroup className="mb-3">
+                    <InputGroup.Prepend>
+                        <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <FormControl
+                        placeholder="Username"
+                        aria-label="Username"
+                        aria-describedby="basic-addon1"
+                    />
+                </InputGroup>
             </div>
         )
     }
