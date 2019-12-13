47796a0ef50858f6cb847e14aa91e7e8840b847a
Gianmarco <gianmarco.caputo@wearetwogether.com>
2019-12-13 12:54:37 +0000

    Prevent pm-text and autoPopulate to appear at the same time

--- src/components/home/textEditor/submitButton.jsx
+++ src/components/home/textEditor/submitButton.jsx
@@ -3,38 +3,59 @@ import { Button } from 'react-bootstrap';
 
 class SubmitButton extends Component {
 
-    // Fetch all the relevant information from the mainState and build the final tag
-    buildTag = () => {
-        // Opening tag
-        let tag = "<";
-        tag += this.props.mainState.tagContent.enclosingTag;
-        console.log(this.props.mainState.tagContent.pmText)
+    // Return a pm-text="" tag with the relevant attribute value
+    buildAutoPopulatedPmText = () => {
+        let snippet = " pm-text=\"";
+        if (this.props.mainState.tagContent.autoPopulateText === "Company Name") {
+            snippet += "companyname"
+        }
+        if (this.props.mainState.tagContent.autoPopulateText === "Company Website") {
+            snippet += "companyurl"
+        }
+        if (this.props.mainState.tagContent.autoPopulateText === "Company Email") {
+            snippet += "companyemail"
+        }
+        if (this.props.mainState.tagContent.autoPopulateText === "Company Phone") {
+            snippet += "companyphone"
+        }
+        if (this.props.mainState.tagContent.autoPopulateText === "Company Address") {
+            snippet += "companyaddress"
+        }
+        if (this.props.mainState.tagContent.autoPopulateText === "Link to LP") {
+            snippet += "landingpage"
+        }
+        snippet += "\""
+        return snippet;
+    }
 
+    // Return a pm-text="tag" with the user-added attribute
+    buildManualPmText = () => {
+        let snippet = " pm-text=\"";
+        snippet += this.props.mainState.tagContent.pmText;
+        snippet += "\""
+        return snippet;
+    }
+
+    // Build the opening tag and its relevant attributes
+    buildOpeningTag = () => {
+        let snippet = "<";
+        snippet += this.props.mainState.tagContent.enclosingTag;
         // If the pm-text is set to Auto Populate
-        if (this.props.mainState.tagContent.autoPopulateText !== null || this.props.mainState.tagContent.autoPopulateText !== "None") {
-            tag += " pm-text=\"";
-            if (this.props.mainState.tagContent.autoPopulateText === "Company Name") {
-                tag += "companyname"
-            }
-            if (this.props.mainState.tagContent.autoPopulateText === "Company Website") {
-                tag += "companyurl"
-            }
-            if (this.props.mainState.tagContent.autoPopulateText === "Company Phone") {
-                tag += "companyphone"
-            }
-            if (this.props.mainState.tagContent.autoPopulateText === "Company Address") {
-                tag += "companyaddress"
-            }
-            if (this.props.mainState.tagContent.autoPopulateText === "Link to LP") {
-                tag += "landingpage"
-            }
-            tag += "\""
+        if (this.props.mainState.tagContent.autoPopulateText) {
+            snippet += this.buildAutoPopulatedPmText();
         }
+        // If the pm-text is NOT set to Auto Populate
+        if (!this.props.mainState.tagContent.autoPopulateText && this.props.mainState.tagContent.pmText) {
+            snippet += this.buildManualPmText();
+        }
+        snippet += " >"
+        return snippet;
+    }
 
-        if (this.props.mainState.tagContent.autoPopulateText === null || this.props.mainState.tagContent.autoPopulateText === "None") {
-            tag += this.props.mainState.tagContent.pmText;
-        }
-        tag += " >"
+    // Build the final tag
+    buildFinalTag = () => {
+        let tag = this.buildOpeningTag();
+
         // Text content
         if (this.props.mainState.tagContent.customText !== null) {
             tag += this.props.mainState.tagContent.customText;
@@ -48,7 +69,7 @@ class SubmitButton extends Component {
         return (
             <Button onClick={(e) => {
                 e.preventDefault();
-                this.buildTag();
+                this.buildFinalTag();
             }} variant="success" type="submit">
                 Generate
             </Button>
