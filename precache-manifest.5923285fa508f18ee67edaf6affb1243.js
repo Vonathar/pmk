9e068e33321e9a4402adfa0399f27200aba38db5
Gianmarco <gianmarco.caputo@wearetwogether.com>
2019-12-13 13:24:50 +0000

    Updates

---
+++ precache-manifest.5923285fa508f18ee67edaf6affb1243.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "909cea7e304f772418698c691f9ee630",
+    "url": "/pmk/index.html"
+  },
+  {
+    "revision": "cad1445ad42c94f25f86",
+    "url": "/pmk/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "6c2894c8463160e12e7a",
+    "url": "/pmk/static/css/main.11174d0c.chunk.css"
+  },
+  {
+    "revision": "cad1445ad42c94f25f86",
+    "url": "/pmk/static/js/2.a2439ae1.chunk.js"
+  },
+  {
+    "revision": "b15a737ad2cfd16685fd27e8e85315d2",
+    "url": "/pmk/static/js/2.a2439ae1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6c2894c8463160e12e7a",
+    "url": "/pmk/static/js/main.66ef5e41.chunk.js"
+  },
+  {
+    "revision": "280fcd28cf0239adfdc6",
+    "url": "/pmk/static/js/runtime-main.ce00c769.js"
+  }
+]);
